9f4a60cf0903fba72898668b92ddfc4de1a7f5b8
githubkeerthi <skvr.java@gmail.com>
2019-10-11 18:20:42 +0530

    Login page setup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+username : string = 'in28minutes'
+  password : string = 'dummy'
+  errorMessage : String = 'Invalid Credentials'
+  invalidLogin : Boolean = false
   constructor() { }
 
   ngOnInit() {
